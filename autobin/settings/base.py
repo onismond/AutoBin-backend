69a59b3f6d6cc12b4841df8b6b3587ad8fb07206
Onismond <67899826+onismond@users.noreply.github.com>
2025-03-15 12:14:57 +0000

    Add GitHub workflow

--- autobin/settings/base.py
+++ autobin/settings/base.py
@@ -103,7 +103,7 @@ JAZZMIN_SETTINGS = {
     "site_icon": None,
 
     # Welcome text on the login screen
-    "welcome_sign": "Welcome to Autobin Admin",
+    "welcome_sign": "Welcome to Autobin Adminn",
 
     # Copyright on the footer
     "copyright": "Autobin UCC",
