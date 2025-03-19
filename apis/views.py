5e61445837760504d6503f80a77bf103f483fd64
Onismond <67899826+onismond@users.noreply.github.com>
2025-03-19 21:34:38 +0000

    Update

--- apis/views.py
+++ apis/views.py
@@ -28,11 +28,12 @@ class HomeView(APIView):
                     pickup_amount += pickup.amount
                 else:
                     uncleared_pickups.append(pickup)
-            for transaction in user.transactions.all():
-                total_paid += transaction.amount
+        transactions = user.transactions.all()
+        for transaction in transactions.filter(cleared=True):
+            total_paid += transaction.amount
         bin_serializer = BinSerializer(bins, many=True)
         pickups_serializer = PickupSerializer(uncleared_pickups, many=True)
-        transaction_serializer = TransactionSerializer(user.transactions.filter(cleared=True), many=True)
+        transaction_serializer = TransactionSerializer(transactions[5], many=True)
         return Response({
             'due_amount': (pickup_amount - total_paid),
             'bins': bin_serializer.data,
