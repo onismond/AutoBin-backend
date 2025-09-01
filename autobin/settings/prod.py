fb4670d54754ee0e10504de670f5c27a93bc930f
Onismond <onismondyao@gmail.com>
2025-09-01 07:47:49 +0000

    Update

--- autobin/settings/prod.py
+++ autobin/settings/prod.py
@@ -13,7 +13,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": ["redis://redis:6379"],
+            "hosts": [("redis", 6379)],
         },
     },
 }
